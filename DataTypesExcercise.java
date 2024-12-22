3183656e15515b9e9b6a5d730a4960b64fcb0700
Ichiro Yamazaki <16699009+ichiroyamazaki@users.noreply.github.com>
2024-12-23 00:33:08 +0800

    Create DataTypesExcercise.java

---
+++ DataTypesExcercise.java
@@ -0,0 +1,12 @@
+public class DataTypesExcercise {
+    public static void main(String[] args) {
+        String firstName = "Ichiro";
+        String lastName = "Yamazaki";
+        char mi = 'P';
+        int faveNumber = 1;
+        String faveCartChar = "Pikachu from Pokemon!";
+   System.out.println("Hello! My name is"+ " " +firstName + " " +mi+"."+ " " +lastName+".");
+   System.out.println("Number"+ " " +faveNumber+ " "+"is my favorite number.");
+   System.out.println("And I love"+ " " +faveCartChar);
+    }
+}
